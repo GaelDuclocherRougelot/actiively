12cf70683d6239ffed6b54224dbb88ee7f2035e7
MeddyFerry <Meddyferry@gmail.com>
2022-10-28 16:23:10 +0200

    create router dom, semantic UI button, repo cleaning

---
+++ front/src/component/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from '../Header/header';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+       <Routes>
+        
+          <Route  />
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/component/Header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <button class="ui button">connexion</button>
+      
+    </div>
+  );
+}
+
+export default Header;
