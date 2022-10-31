39e873a132b6851829b8f73a2f0ee7f54ef6e4fe
Alexa Dupin <alexa.dupin@gmail.com>
2022-10-31 15:47:11 +0100

    Added routes to App

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import Activity from "./components/Activity/Activity";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 import './styles/index.scss'
 
@@ -8,7 +9,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Activity />
+      <Routes>
+        <Route
+          path="/activity/:id"
+          element={<Activity />}
+        />
+      </Routes>
       <Footer />
     </div>
   );
