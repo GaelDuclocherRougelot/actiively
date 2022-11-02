2760e14693f5044a1a5e82c1e6138311daaa3bf1
Alexa Dupin <alexa.dupin@gmail.com>
2022-11-02 15:17:56 +0100

    Eslint fixes

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import './footerStyles.scss';
 
 function Footer() {
-return (
-    <footer className='footer'>
-        <h1 className='footer-title'>Copyright 2022</h1>
+  return (
+    <footer className="footer">
+      <h1 className="footer-title">Copyright 2022</h1>
     </footer>
-);
+  );
 }
 
 export default React.memo(Footer);
--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <FontAwesomeIcon icon={faLightbulb} size="3x" />
       <h1 className="appheader-title">Actiively</h1>
       {/* Login button will become an icon on mobile */}
-            <button className="appheader-login">Connexion</button>
+      <button type="button" className="appheader-login">Connexion</button>
       <FontAwesomeIcon icon={faUser} size="2x" className="appheader-login-mobile" />
     </header>
   );
