6eb49645900d8c050c7509666c0d4900ef3ddf6c
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2022-11-04 17:16:15 +0100

    feat: add some doc swagger on route api/v1/activity/:id & modif on custom api error

--- back/app/errors/apiErrors.js
+++ back/app/errors/apiErrors.js
@@ -1,7 +1,22 @@
+/**
+ * On implémente notre propre type d'erreur.
+ * Cela s'appelle une exception
+ * On en profite pour lui permettre de prendre
+ * des information supplémentaires, par exemple : un code de status HTTP
+ * Les instances transporteront ces infos avec elles
+ * et elles pourront être exploitées ultérieurement
+ * @typedef {object} ApiError
+ * @property {string} message - Error message
+ * @property {string} name - Error name
+ * @property {object} infos - Additionnal informations
+ */
 module.exports = class CustomApiError extends Error {
-    constructor(message, status = 500) {
-      // super permet d'appeler le constructeur de la classe parente
-      super(message);
-      this.status = status;
-    }
+  constructor(message, infos) {
+    // On envoi le message à la classe mère Error
+    super(message);
+    // On défini le nom de l'erreur qui de base est 'Error'
+    this.name = 'ApiError';
+    // on défini les infos supplémentaires afin des les transporter
+    this.infos = infos;
+}
 }
