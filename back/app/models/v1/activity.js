e7acf3fb85dd5271ae0dec04d3a3949416deb874
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2022-11-04 16:02:17 +0100

    modif: add items in requests activity

--- back/app/models/v1/activity.js
+++ back/app/models/v1/activity.js
@@ -8,8 +8,8 @@ module.exports = {
     async findByPk(id){
         const result = await client.query(`
             SELECT 
-                a.name, a.address, a.zip_code, d.name as day, d.start_time, d.end_time, a.price, a.price_type, a.gender, a.level, a.description,
-                json_build_object('email', o.contact_email, 'phone_number', o.phone_number, 'organism_description', o.description) as organism_infos 
+                a.name, a.address, a.zip_code, a.city, d.name as day, d.start_time, d.end_time, a.price, a.price_type, a.gender, a.level, a.description, a.image_url,
+                json_build_object('email', o.contact_email,'name', o.name, 'phone_number', o.phone_number, 'organism_description', o.description) as organism_infos 
             FROM activity a
                 JOIN day d ON pk_activity = code_activity
                 JOIN organism o ON pk_activity = code_activity
@@ -22,8 +22,9 @@ module.exports = {
 
     async findByKeyword(q){
         const result = await client.query(`
-            SELECT a.code_activity, a.name, a.zip_code, d.name as day, a.price, a.price_type from "activity" a
+            SELECT a.code_activity, a.name as activity_name, o.name as organism_name, a.zip_code, a.city, d.name as day, a.price, a.price_type, a.image_url from "activity" a
             JOIN day d ON pk_activity = code_activity
+            JOIN organism o ON pk_organism = o.email
             WHERE a.name SIMILAR TO ($1)
             AND a.zip_code SIMILAR TO ($2)
         `, [q.keyword, q.zip_code]);
