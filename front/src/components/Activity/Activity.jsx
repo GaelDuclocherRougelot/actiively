1ed0086b614dd671be7f6fc713731df718551459
Alexa Dupin <alexa.dupin@gmail.com>
2022-11-06 16:00:14 +0100

    Axios request to get data for activity/:id + corrected model in the Back for activity

--- front/src/components/Activity/Activity.jsx
+++ front/src/components/Activity/Activity.jsx
@@ -1,63 +1,94 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import {
   Image, Grid, Header, Container, Label, Icon,
 } from 'semantic-ui-react';
 
-function Activity({
-  data,
-}) {
+function Activity() {
+  const [activity, setActivity] = useState({});
+  const [organism, setOrganism] = useState({});
+
+  // Used params to add id to URL when sending an axios request
+  let id = useParams();
+  // Transformed result to number to match format set in the Back
+  id = Number(id.id);
+
+  // Request to API to get data for an Activity with an id in URL
+  const fetchActivity = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/api/v1/activity/${id}`);
+      // Update states with results
+      setActivity(response.data);
+      setOrganism(response.data.organism_infos);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect so that data is fetched on mount
+  useEffect(
+    () => {
+      fetchActivity();
+    },
+    [],
+  );
+
+  // console.log(activity);
+  // console.log(organism);
+
   return (
     <Container>
       <Grid centered>
         <Grid.Row>
-          <Header as="h1">{data.name}</Header>
+          <Header as="h1">{activity.name}</Header>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column mobile={12} tablet={8} computer={4}>
-            <Image src={data.image_url} alt={data.name} />
+            <Image src={activity.image_url} alt={activity.name} />
           </Grid.Column>
           <Grid.Column mobile={16} tablet={8} computer={4}>
             <Container style={{ textAlign: 'center' }}>
               <br />
-              {data.address}
+              {activity.address}
               <br />
-              {data.zip_code}
+              {activity.zip_code}
               {' '}
-              {data.city}
+              {activity.city}
               <br />
               <br />
               <Label.Group color="teal">
                 <Label as="a">
-                  {data.price}
+                  {activity.price}
                   {' '}
                   €
                   {' '}
-                  {data.price_type}
+                  {activity.price_type}
                 </Label>
                 <Label as="a">
-                  {data.gender}
+                  {activity.gender}
                 </Label>
                 <Label as="a">
-                  {data.level}
+                  {activity.level}
                 </Label>
                 <Label as="a">
-                  {data.day}
+                  {activity.day}
                   {' '}
-                  {data.start_time}
+                  {activity.start_time}
                   {' '}
                   -
                   {' '}
-                  {data.end_time}
+                  {activity.end_time}
                 </Label>
               </Label.Group>
               <Header as="h3" size="small">Informations de contact</Header>
-              {data.organism_infos.email}
+              {organism.email}
               <Icon name="mail" />
               <br />
-              {data.organism_infos.phone_number}
+              {organism.phone_number}
               <Icon name="phone" />
             </Container>
           </Grid.Column>
@@ -65,7 +96,7 @@ function Activity({
         <Grid.Row>
           <Grid.Column mobile={12} computer={8}>
             <Header as="h2" size="medium">L&apos;activité</Header>
-            {data.description}
+            {activity.description}
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
@@ -73,9 +104,9 @@ function Activity({
             <Header as="h2" size="medium">
               L&apos;association :
               {' '}
-              {data.organismName}
+              {organism.name}
             </Header>
-            {data.organism_infos.organism_description}
+            {organism.organism_description}
             {' '}
 
           </Grid.Column>
@@ -84,29 +115,5 @@ function Activity({
     </Container>
   );
 }
-
-Activity.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    image_url: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
-    zip_code: PropTypes.string.isRequired,
-    city: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    price_type: PropTypes.string.isRequired,
-    gender: PropTypes.string.isRequired,
-    level: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    organismName: PropTypes.string.isRequired,
-    phone_number: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    organism_description: PropTypes.string.isRequired,
-    day: PropTypes.string.isRequired,
-    start_time: PropTypes.string.isRequired,
-    end_time: PropTypes.string.isRequired,
-  }).isRequired).isRequired,
-};
-
-Activity.defaultProps = {};
 
 export default React.memo(Activity);
