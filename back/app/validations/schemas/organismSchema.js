42da0820414876a8d034c165b80da7da17e65ab3
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2022-11-08 10:04:49 +0100

    add new data in json files & fix phone regex

--- back/app/validations/schemas/organismSchema.js
+++ back/app/validations/schemas/organismSchema.js
@@ -6,5 +6,5 @@ module.exports = joi.object({
     password: joi.string().min(3).max(128).required().pattern(new RegExp('^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$')),
     description: joi.string(),
     contact_email: joi.string().pattern(new RegExp('(.+)@(.+){2,}\.(.+){2,}')).min(3).max(128),
-    phone_number: joi.string().pattern(new RegExp('^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$'))
+    phone_number: joi.string().pattern(new RegExp('^(?:(?:(?:\\+|00)33[ ]?(?:\\(0\\)[ ]?)?)|0){1}[1-9]{1}([ .-]?)(?:\d{2}\1?){3}\d{2}$'))
 });
