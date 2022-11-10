8ac4d94e0ad5f44a98cdefc77e429e86db796479
MeddyFerry <Meddyferry@gmail.com>
2022-11-10 10:36:10 +0100

    merge filters branch + correction .env

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -65,7 +66,7 @@ function App() {
             <Home
               handle={handleClick}
             />
-        )}
+          )}
         />
         <Route
           path="/activity"
