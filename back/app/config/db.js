43fc015818420f0ba8200eeb8cdb07c51bf4844a
AlaaEddineRAHALI <ralaaeddine1@gmail.com>
2022-11-27 22:04:40 +0100

    fix2

--- back/app/config/db.js
+++ back/app/config/db.js
@@ -15,7 +15,7 @@ const debug = require('debug')('SQL:log');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'localhost',//'gaelduclocherrougelot-server.eddi.cloud', // or 'localhost' 
+    host: 'gaelduclocherrougelot-server.eddi.cloud', // or 'localhost' 
     user: 'actiively', //'actiively' //
     max: 20,
     idleTimeoutMillis: 30000,
