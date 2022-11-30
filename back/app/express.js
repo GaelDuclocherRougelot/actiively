97bed28aed5589dd8ee178952503586e02881c5a
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2022-11-30 15:36:45 +0100

    fix: line 9 in express.js (path-to-regex error)

--- back/app/express.js
+++ back/app/express.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-require('./helpers/apiDocs')(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
