3eec6a60f81899d0c114871ecf4f3a4d92e68a51
GaelDuclocherRougelot <gaelduclocher.rougelot@gmail.com>
2023-01-19 11:19:27 +0100

    rm cookie-parser

--- back/app/express.js
+++ back/app/express.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routers');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -16,7 +15,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(cookieParser());
 app.use(router);
 
 module.exports = app
